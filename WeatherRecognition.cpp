7f0686b5d8d6682e11d93ac206c0a88a2aa4b66a
Denis <denispuscas1@gmail.com>
2024-03-11 20:09:31 +0200

    test function

--- WeatherRecognition.cpp
+++ WeatherRecognition.cpp
@@ -42,10 +42,21 @@ set<string> testSet;
 set<string> trainSet;
 set<string> imgSet;
 
+int nrImg;
+
+void wait() {
+	int c;
+	cout << endl << "Press enter to continue.";
+	while ((c = getchar()) != '\n' && c != EOF);
+	getchar();
+}
+
 void openImages()
 {
 	char file[MAX_PATH];
 	char subfolder[MAX_PATH];
+
+	nrImg = 0;
 
 	for (int i = 0; i < sizeof(tagName) / sizeof(char*); i++) {
 		strcpy(subfolder, folderPath);
@@ -57,6 +68,7 @@ void openImages()
 		FileGetter fg(subfolder, "jpg");
 		while (fg.getNextAbsFile(file))
 		{
+			nrImg++;
 			imgSet.insert(file);
 		}
 
@@ -69,13 +81,42 @@ void openImages()
 				testSet.insert(img);
 		}
 	}
-	cout << "Train set size: " << trainSet.size() << endl;
-	cout << "Test set size: " << testSet.size() << endl;
+}
+
+void verifyNbOfImages() {
+	openImages();
+	if (nrImg == 6862) {
+		printf("\nTest verify nb of images successfully\n");
+	}
+	else {
+		printf("\nYou made a mistake in counting the files\n");
+	}
 }
 
 int main() 
 {
-	openImages();
+	int op;
+	do
+	{
+		system("cls");
+		printf("Menu:\n");
+		printf(" 1 - Main program\n");
+		printf(" 2 - Testing number of images\n");
+		printf("Option: ");
+		scanf("%d", &op);
+
+		switch (op)
+		{
+		case 1:
+			openImages();
+			break;
+		case 2:
+			verifyNbOfImages();
+			break;
+		}
+		wait();
+
+	} while (op != 0);
 
 	return 0;
 }
